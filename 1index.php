df49426f73fa53c9ed264bd112378fb38cc17732
MayTomaláZ <86171053+Maick1@users.noreply.github.com>
2024-03-25 08:40:40 -0500

    Initial commit

---
+++ 1index.php
@@ -0,0 +1,21 @@
+<!-- si se trabaja solo php no importa el cierre
+con html si se debe cerrar la etiqueta de php -->
+<?php
+echo "\nHola Mundo";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "<p>Hola Mundo desde PHP.</p>";
+echo "<p>Este es un ejemplo de salto de línea en PHP.</p>";
+?>
+
+</body>
+</html>
